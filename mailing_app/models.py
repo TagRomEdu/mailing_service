6775fa1ad8a3768b656ed10bf0e4d38d1adf6468
TagRomEdu <qwertystyles@gmail.com>
2023-10-31 15:28:55 +0500

    added model Client

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name='E-mail')
+    name = models.CharField(max_length=250, verbose_name='ФИО')
+    comment = models.TextField(**NULLABLE, verbose_name='Комментарий')
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
