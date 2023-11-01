90ff69e0b5727352aa695ef78987ad071edd04b0
TagRomEdu <qwertystyles@gmail.com>
2023-11-01 15:47:44 +0500

    added models to admin panel

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing_app.models import Client, Mailing, Message
+from users_app.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'phone')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    list_filter = ('name',)
+    list_search = ('name',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('mailing_time', 'period', 'status')
+    list_filter = ('period', 'status')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'body')
+    list_filter = ('subject', 'body')
+    list_search = ('subject', 'body')
+
