4ca172662cb61e55b6f07a542d025bfd3f722735
TagRomEdu <qwertystyles@gmail.com>
2023-11-01 15:47:08 +0500

    changed verbose name

--- mailing_app/apps.py
+++ mailing_app/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MailingAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing_app'
-    verbose_name = 'Рассылки'
+    verbose_name = 'Сервис рассылок'
