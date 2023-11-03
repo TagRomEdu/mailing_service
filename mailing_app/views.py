5bd638aa8019ce8daf71bb0b0394d7f82eb00feb
TagRomEdu <qwertystyles@gmail.com>
2023-11-03 11:46:16 +0500

    added blog's views

--- mailing_app/views.py
+++ mailing_app/views.py
@@ -1,5 +1,84 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from pytils.translit import slugify
+
+from mailing_app.forms import BlogForm
+from mailing_app.models import Blog
 
 
-def homepage(request):
-    return render(request, 'mailing_app/blog.html')
+def blog_single(request):
+    return render(request, 'mailing_app/blog-single.html')
+
+
+def browsejobs(request):
+    return render(request, 'mailing_app/mailing_list.html')
+
+
+def contact(request):
+    return render(request, 'mailing_app/contact.html')
+
+
+def index(request):
+    return render(request, 'mailing_app/index.html')
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy('mailing_app:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.name)
+            new_blog.user = self.request.user
+            new_blog.save()
+        return super().form_valid(form)
+
+
+class BlogListView(ListView):
+    model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy('mailing_app:blog_list')
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.object.user != self.request.user:
+            raise Http404
+        return self.object
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.name)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('mailing_app:blog_list', args=[self.kwargs.get('slug')])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('mailing_app:blog_list')
