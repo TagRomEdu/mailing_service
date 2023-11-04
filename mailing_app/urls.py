e05bea8ab29d57fe165f735fc98249dd05dace27
TagRomEdu <qwertystyles@gmail.com>
2023-11-04 11:25:48 +0500

    added mailing list and mailing detail to urls

--- mailing_app/urls.py
+++ mailing_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from mailing_app.apps import MailingAppConfig
-from mailing_app.views import blog_single, browsejobs, contact, index, BlogListView
+from mailing_app.views import blog_single, contact, index, BlogListView, MailingListView, BlogDetailView, \
+    MailingDetailView
 
 app_name = MailingAppConfig.name
 urlpatterns = [
     path('', index, name='index'),
     path('blog/', BlogListView.as_view(), name='blog_list'),
-    path('blog_single/', blog_single, name='blog_single'),
-    path('browsejobs/', browsejobs, name='browsejobs'),
+    path('blog_single/', BlogDetailView.as_view(), name='blog_single'),
+    path('mailing_list/', MailingListView.as_view(), name='mailing_list'),
+    path('mailing_single/', MailingDetailView.as_view(), name='mailing_single'),
     path('contact/', contact, name='contact'),
 ]
