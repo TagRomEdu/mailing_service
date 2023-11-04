62b08aa8c866db36738c352c32c5b3e8f97b4569
TagRomEdu <qwertystyles@gmail.com>
2023-11-04 11:35:58 +0500

    added form for mailing

--- mailing_app/forms.py
+++ mailing_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from mailing_app.models import Blog
+from mailing_app.models import Blog, Mailing
 
 
 class BlogForm(forms.ModelForm):
@@ -8,6 +8,18 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('name', 'text', 'preview', 'is_published')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class MailingForm(forms.ModelForm):
+
+    class Meta:
+        model = Mailing
+        fields = ('period', 'status', 'message', 'clients')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
