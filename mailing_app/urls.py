be29defc32e291b79ad7f3d828d22292c5977dc9
TagRomEdu <qwertystyles@gmail.com>
2023-11-04 11:52:07 +0500

    added urls for creating and updating blogs

--- mailing_app/urls.py
+++ mailing_app/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 
 from mailing_app.apps import MailingAppConfig
 from mailing_app.views import contact, index, BlogListView, MailingListView, BlogDetailView, \
-    MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView
+    MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, BlogDeleteView, BlogCreateView, \
+    BlogUpdateView
 
 app_name = MailingAppConfig.name
 urlpatterns = [
     path('', index, name='index'),
+    path('blog_create/', BlogCreateView.as_view(), name='create_blog'),
+    path('blog_update/<slug>', BlogUpdateView.as_view(), name='update_blog'),
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/<slug>', BlogDetailView.as_view(), name='blog_single'),
+    path('blog/<slug>/confirm_delete', BlogDeleteView.as_view(), name='delete_blog'),
     path('mailing_create/', MailingCreateView.as_view(), name='create_mailing'),
     path('mailing_update/<int:pk>', MailingUpdateView.as_view(), name='update_mailing'),
     path('mailing_list/', MailingListView.as_view(), name='mailing_list'),
