ac21d9154bf55615d34d484c56746626edc8f7f7
TagRomEdu <qwertystyles@gmail.com>
2023-11-07 09:43:19 +0500

    changed on_delete for field mailing in message

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -57,7 +57,7 @@ class Message(models.Model):
     subject = models.CharField(max_length=150, verbose_name='Тема письма')
     body = models.TextField(verbose_name='Тело письма')
     user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
-    mailing = models.ForeignKey(Mailing, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Рассылка')
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, **NULLABLE, verbose_name='Рассылка')
 
     def __str__(self):
         return f'{self.subject}'
