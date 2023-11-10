0800ef61d7c5ddd0eb1f24b5d55995d518810096
TagRomEdu <qwertystyles@gmail.com>
2023-11-10 12:05:22 +0500

    added func for caching client's list

---
+++ mailing_app/services.py
@@ -0,0 +1,16 @@
+from django.core.cache import cache
+
+from mailing_app.models import Client
+from config import settings
+
+
+def get_clients_cache():
+    if settings.CACHE_ENABLED:
+        key = 'client_list'
+        client_list = cache.get(key)
+        if client_list is None:
+            client_list = Client.objects.all()
+            cache.set(key, client_list)
+    else:
+        client_list = Client.objects.all()
+    return client_list
